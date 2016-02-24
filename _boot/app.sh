7bc347a81867828d268a69b58d768655658eaad0
paul shorey <ps@artspaces.net>
2016-02-24 13:33:44 -0500

    port

--- _boot/app.sh
+++ _boot/app.sh
@@ -1,10 +1,10 @@
-cd /www/$(hostname)
-git pull
-npm install
-i=0;
-while true; do
-	i=$[$i+1]
-	i=$[$i+1]
-	casperjs bot.js --iteration=$i
-	sleep 60
-done
+# cd /www/$(hostname)
+# git pull
+# npm install
+# i=0;
+# while true; do
+# 	i=$[$i+1]
+# 	i=$[$i+1]
+# 	casperjs bot.js --iteration=$i
+# 	sleep 60
+# done
