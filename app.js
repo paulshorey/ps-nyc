a504696dfaba8ffd13d42ebb93eafbce21ae1ab9
paul (local) <ps@artspaces.net>
2016-06-07 11:31:47 -0400

    quick fix broken rainbow

--- app.js
+++ app.js
@@ -1,46 +1,113 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 4080;
+// var pro = process;
+// process.inc = {};
+// process.inc.express = require('express');
+// process.inc.express_parser = require('body-parser');
+// // modules
+// process.moment = require('moment'); // process.moment(new Date(2011, 9, 16)).
+// process.moment.now = process.moment();
+// // process.request = require('request');
+// process.fs = require('fs');
+// process.http = require('http');
+// process.https = require('https');
+// process.q = require('q');
+// process._ = require('underscore');
+// process.contentful = require('contentful');
+// process.ejs = require("ejs");
+// // process.o3o = require('o3o');
+// // process.o3o.tags = process.o3o();
+// process.os = require("os");
+// // env
+// process.env.PORT = 4080;
+// process.env.PATH = __dirname;
 
-http.createServer(function(request, response) {
+// // app
+// process.app = process.inc.express();
+// process.app.use(process.inc.express_parser.json({
+// 	limit: '50mb'
+// }));
+// process.app.use(process.inc.express_parser.urlencoded({
+// 	limit: '50mb',
+// 	extended: true
+// }));
+// // custom
+// process.fun = require("./node_custom/fun.js");
+// process.console = require("./node_custom/console.js").console; // uses process.app
+// process.response = require("./node_custom/response.js");
+// // secret
+// //process.secret = require('../secret-nyc/all.js');
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+// //////////////////////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////////////////////
+// // VIEW
+// process.window = {};
+// process.window.server = {};
+// process.window.server.platform = process.os.platform();
 
-  var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-  };
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// INDEX
+// var index = process.ejs.render(process.fs.readFileSync('./www/index.html', 'utf-8'));
+// var loadmore = process.ejs.render(process.fs.readFileSync('./www/loadmore.html', 'utf-8'));
 
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+// process.app.get('loadmore', function(request, response){
+// 	response.setHeader('Content-Type', 'text/html'); 
+// 	response.writeHead(200);
+// 	response.write(process.ejs.render(process.fs.readFileSync('./html/loadmore.html', 'utf-8')));
+// 	response.end();
+// });
+// process.app.get('', function(request, response){
+// 	response.setHeader('Content-Type', 'text/html'); 
+// 	response.writeHead(200);
+// 	response.write(process.ejs.render(process.fs.readFileSync('./html/index.html', 'utf-8')));
+// 	response.end();
+// });
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// ASSETS
+// process.app.use(process.inc.express.static('html_assets'));
+// process.app.use(function(err, req, res, next) {
+// 	console.error(err.stack);
+// 	res.status(500).send('Something broke!');
+// });
+var static = require('node-static');
+var fileServer = new static.Server('./www');
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file 
+                console.error("Error serving " + request.url + " - " + err.message);
+                response.writeHead(err.status, err.headers);
+                response.end();
+            } else {
+	          response.end();
+	      }
+        });
+    }).resume();
+}).listen(8888);
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// RELOAD
+// process.fs.watch('www', function (event, filename) {
+// 	process.console.warn('file changed:', filename);
+// 	process.exit();
+// });
+// var watch = require('watch')
+// watch.createMonitor('/home/mikeal', function (monitor) {
+//   monitor.files['/home/mikeal/.zshrc'] // Stat object for my zshrc.
+//   monitor.on("created", function (f, stat) {
+//     // exec reload command here
+//   })
+//   monitor.on("changed", function (f, curr, prev) {
+//     // exec reload command here
+//   })
+//   monitor.on("removed", function (f, stat) {
+//     // exec reload command here
+//   })
+// })
 
-      var headers = {};
-      var contentType = contentTypesByExtension[path.extname(filename)];
-      if (contentType) headers["Content-Type"] = contentType;
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// start
+// var httpServer = process.http.createServer(process.app);
+// httpServer.listen(process.env.PORT);
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+// var httpsServer = process.https.createServer({key: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/privkey.pem', 'utf8'), cert: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/fullchain.pem', 'utf8')}, process.app);
+// httpsServer.listen(443);
