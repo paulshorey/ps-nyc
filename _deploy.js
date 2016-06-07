78e4864f7c5f275ab5b90d043d1c81aee4f45487
paul (local) <ps@artspaces.net>
2016-06-07 13:02:28 -0400

    test autodeploy

--- _deploy.js
+++ _deploy.js
@@ -13,6 +13,7 @@ app.all('/_deploy', function(req, res) {
 	});
 
 	// apply
+	console.log('_deploy webhook received');
 	var spawn = require('child_process').spawn,
 		deploy = spawn('bash', ['_deploy.sh']);
 
