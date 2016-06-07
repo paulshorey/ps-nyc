995e453b22de8e41fba9edc1f5e979d3f66a78aa
paul (local) <ps@artspaces.net>
2016-06-07 16:55:13 -0400

    paul files

--- _boot/app.sh
+++ _boot/app.sh
@@ -2,11 +2,11 @@
 
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 4080
 
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/ps1-git
-# cd /www/ps-nyc
-# git reset HEAD -\-hard;
-# git pull
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/ps1-git
+cd /www/ps-nyc
+git reset HEAD -\-hard;
+git pull
 
 # rm -rf public/console
 # http-server www -p 4080 -s -c 30 #-d false
--- _deploy.sh
+++ _deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/ps1-git
-cd /www/ps-nyc
-git reset HEAD -\-hard;
-git pull
 
 killall node
-reboot
+
+bash _boot/app.sh
+bash _boot/deploy.sh
