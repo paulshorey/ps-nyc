859b77dcef870d8c15d9fe98779f1e0e4dcf0626
paul (local) <ps@artspaces.net>
2016-07-01 09:50:50 -0400

    homepage text

---
+++ www/square 2/clock/gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+// var bower = require('bower');
+// var babel = require('gulp-babel');
+var scss = require('gulp-sass');
+var compass = require('gulp-compass');
+// var sh = require('shelljs');
+var autoprefixer = require("gulp-autoprefixer");
+var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var bower = require('bower-files')();
+var jshint = require('gulp-jshint');
+
+var path = {
+  scss: {
+    src: ['./scss/*.scss', '!./scss/all.scss'],
+    dst: './css'
+  },
+  css: {
+    src: ['./css/*.css', '!./css/all.css'],
+    dst: './css'
+  },
+  js: {
+    src: ['./js/*.js', '!./js/all.js'],
+    dst: './js'
+  }
+};
+
+
+gulp.task('default', ['scss','lint_js']);
+
+// gulp.task('css', ['scss','concat_css']);
+
+// gulp.task('lint', ['lint_js']);
+
+gulp.task('watch', function () {
+  gulp.watch(path.scss.src, ['scss']);
+  gulp.watch(path.js.src, ['lint_js']);
+});
+
+
+gulp.task('scss', function (done) {
+  gulp.src(path.scss.src)
+    .pipe(scss({compass:true}))
+    .on('error', scss.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(path.scss.dst))
+    .on('end', done);
+});
+gulp.task('lint_js', function() {
+  return gulp.src(path.js.src)
+    .pipe(jshint(['.jshintignore']))
+    .pipe(jshint.reporter('jshint-stylish', {beep: true}));
+});
+
+
+gulp.task('concat_css', function () {
+  return gulp.src(path.css.src)
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest(path.css.dst));
+});
+gulp.task('concat_js', function () {
+  return gulp.src(path.js.src)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest(path.js.dst));
+});
---
+++ www/square 2/snake/README.md
@@ -0,0 +1,24 @@
+ORIGINAL APP BY:
+JavaScript Snake
+By Patrick Gillespie
+http://patorjk.com/games/snake
+Source code is available here: https://github.com/patorjk/JavaScript-Snake
+  
+  
+MODIFIED for MOBILE DEVICES + CLICK / TAP controls:
+By Paul Shorey
+http://paulshorey.com
+Tweaked UI, controls, made it work and look better on different size screens, and mainly, added click/tap functionality so it may be played on mobile devices, though the zoom/scale needs to be tuned.
+  
+This was a fun project, still need to:
+	1) adjust the viewport without breaking gameplay
+	2) add "pause" button for mobile
+	3) cleanup all that spaghetti
+  
+  
+
+See it in action:
+http://paulshorey.com/square/snake/
+  
+Compared to original (didn't use mouse / tap, would break on different size screens)
+http://patorjk.com/games/snake
