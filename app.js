d16f09394bcea7e343dd4476c72606f894e7c8e9
paul (local) <ps@artspaces.net>
2016-11-29 00:39:32 -0700

    try prefix name

--- app.js
+++ app.js
@@ -89,13 +89,13 @@ require('http').createServer(function (request, response) {
 
 }).listen(4080);
 
-// test
-var static = require('node-static');
-var fileServer = new static.Server('./www/luxul');
+// LUX
+var LUXstatic = require('node-static');
+var LUXfileServer = new LUXstatic.Server('./www/luxul');
 require('http').createServer(function (request, response) {
 
     request.addListener('end', function () {
-        fileServer.serve(request, response, function (err, result) {
+        LUXfileServer.serve(request, response, function (err, result) {
             if (err) { // There was an error serving the file 
                 console.error("Error serving " + request.url + " - " + err.message);
                 response.writeHead(err.status, err.headers);
