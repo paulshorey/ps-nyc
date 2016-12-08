cf256c36fd20f37c031ac824fa8382bd060621fa
paul (local) <ps@artspaces.net>
2016-12-07 20:37:33 -0700

    remove exra https block, nginx will handle that

--- app.js
+++ app.js
@@ -20,22 +20,3 @@ require('http').createServer(function (request, response) {
     });
 
 }).listen(2080);
-
-require('http').createServer(function (request, response) {
-
-    request.addListener('end', function () {
-        fileServer.serve(request, response, function (err, result) {
-            if (err) { // There was an error serving the file 
-                console.error("Error serving " + request.url + " - " + err.message);
-                response.writeHead(err.status, err.headers);
-                response.end();
-            } else {
-              response.end();
-          }
-        });
-    }).resume();
-    request.setTimeout(1000, function () {
-        request.abort();
-    });
-
-}).listen(443);
