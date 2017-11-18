25bc01b541a7e9d18756d00394fa14a176cc97d4
paul (local) <ps@artspaces.net>
2017-11-18 08:45:21 -0700

    new luxul

---
+++ pluraltest/myApp.js
@@ -0,0 +1,7 @@
+function lowercase(value) {
+  return value.toLowerCase()
+}
+
+module.exports = {
+  lowercase,
+}
--- www/luxul_app/src/components/form/LuxulForm.js
+++
@@ -1,8 +0,0 @@
-import validations from './exports/validations';
-import LuxulForm from './exports/LuxulForm';
-import LuxulInput from './exports/LuxulInput';
-import LuxulSelect from './exports/LuxulSelect';
-import LuxulToggle from './exports/LuxulToggle';
-import LuxulButton from './exports/LuxulButton';
-
-export { validations, LuxulForm, LuxulInput, LuxulSelect, LuxulToggle, LuxulButton };
