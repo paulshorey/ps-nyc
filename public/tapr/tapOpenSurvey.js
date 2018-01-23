e51310711a25e68b645f24c4f311063e7f05731e
paul (local) <ps@artspaces.net>
2018-01-23 16:32:57 -0700

    upload tapr assignment

--- public/tapr/tapOpenSurvey.js
+++ public/tapr/tapOpenSurvey.js
@@ -1,62 +1,122 @@
-(function() {
+// Paul Shorey ---- ps@artspaces.net ---- 385.770.6789 //
 
-	// config
+(function () {
+	"use strict";
+
+	// ---- initial config: ---- //
+
 	var meta = {};
-	meta.id = Math.random();
+	meta.uid = "_" + Math.round(Math.random() * 1000000000000000); // "_" because id attribute can not start with number
+	meta.openOfferWindowName = "offerWindow" + meta.uid;
+	meta.openOfferFunctionName = "offerFunction" + meta.uid;
+	meta.openOfferTimeoutName = "offerTimeout" + meta.uid;
 
 
-	// FORM ACTION
-	var formSubmit = function(e) {
+	// ---- user actions: ---- //
+
+	// OPEN OFFER
+	var userAction_openOffer = function userAction_openOffer(e) {
 		e.preventDefault();
-		console.log('submit...',e);
 
-		// get survey data
-		var postUrl = "https://www.tapresearch.com/supply_api/surveys/offer";
-		var postData = {
-			"api_token": "9a7fb35fb5e0daa7dadfaccd41bb7ad1",
-			"user_identifier": "tapresearch"
+		// JSONP prepare for offer by opening new window
+		window[meta.openOfferWindowName] = window.open("about:blank", "_blank");
+
+		// JSONP handler
+		window[meta.openOfferFunctionName] = function (offerData) {
+
+			// offer data
+			if (offerData && offerData.offer_url) {
+				// offer available :)
+				document.getElementById(meta.uid).innerHTML = "<div>" + "	Survey has been opened.<br />" + "	This survey reward is between " + offerData.message_hash.min + " to  " + offerData.message_hash.max + "  " + offerData.message_hash.currency + "" + "</div>";
+				// open offer url :D
+				window[meta.openOfferWindowName].location.href = offerData.offer_url;
+				window[meta.openOfferWindowName].focus();
+			} else {
+				// no offer available :(
+				document.getElementById(meta.uid).innerHTML = "<div>" + "	Sorry, we have no survey available at this time." + "</div>";
+			}
+
+			// response went through OK - disable timeout scenario
+			window.clearTimeout(window[meta.openOfferTimeoutName]);
 		};
-		console.log('fetching...'+postUrl);
-		fetch( postUrl, {
-			method: 'POST',
-			mode: 'cors',
-			body: JSON.stringify(postData), 
-			headers: new Headers({
-				'Content-Type': 'application/json'
-			})
-		  })
-		  .then(function(response){
-			  console.log('Success:', response)
-		  })
-		  .catch(error => console.error('Error:', error));
-		//   .then(res => res.json())
-		//   .catch(error => console.error('Error:', error))
-		//   .then(function(response){
-		// 	  console.log('Success:', response)
-		//   });
+		
+		// JSONP execute
+		var getUrl = "https://www.tapresearch.com/supply_api/surveys/offer?callback=" + meta.openOfferFunctionName + "&dataType=jsonp&api_token=9a7fb35fb5e0daa7dadfaccd41bb7ad1&user_identifier=tapresearch";
+		var imported = document.createElement('script');
+		imported.src = getUrl;
+		document.head.appendChild(imported);
+		window[meta.openOfferTimeoutName] = setTimeout(window[meta.openOfferFunctionName], 3000);
+	};
+
+	// FORM VALIDATION
+	var formValidate = function formValidate(e) {
+		// form element
+		e.preventDefault();
+		var form = e.target;
+		if (typeof form.action === 'undefined') {
+			form = e.target.form;
+		}
+		// VALIDATE
+		var valid = true;
+		form.querySelectorAll("input[validate]").forEach(function (inputToValidate) {
+			// each
+			if (inputToValidate.value.length < 1 || inputToValidate.value.length > 32) {
+				// FAIL
+				form.submit.disabled = true;
+				inputToValidate.classList.add('invalid');
+				valid = false;
+			} else {
+				// PASS
+				form.submit.disabled = false;
+				inputToValidate.classList.remove('invalid');
+			}
+		});
+		return valid;
+	};
+
+	// FORM SUBMIT ATTEMPT
+	var formSubmit = function formSubmit(e) {
+		if (formValidate(e)) {
+			userAction_openOffer(e);
+		}
 	};
 
 
-	// WRITE OUTPUT - initial placeholder
-	window.document.writeln('<div id="'+ meta.id +'"></div>');
+	// ---- inject form: ---- //
 
-	// make form contents
+	// WRITE OUTPUT - initial placeholder div and stylesheet
+
+	// stylesheet
+	var styleSheet = document.createElement('style');
+	styleSheet.type = 'text/css';
+	var css = "" + "#" + meta.uid + " form {display:flex;max-width: 250px;} " + "#" + meta.uid + " input {border:solid 1px #25BEE4;flex-grow:1;} " + "#" + meta.uid + " input.invalid {border:solid 1px red;} " + "#" + meta.uid + " button {background:#25BEE4;border:none;border-radius:0;display:block;min-width:80px;padding:4px 6px 6px 4px;color:#fff;font-weight:bold;cursor:} " + "#" + meta.uid + " *:focus {outline: none;}" + "";
+	styleSheet.appendChild(document.createTextNode(css));
+	document.head.appendChild(styleSheet);
+	var styleContainer = "" + "font-family: sans-serif;" + "font-size: 15px;" + "line-height: 17px;" + "color: #25BEE4;" + "";
+	window.document.writeln('<div id="' + meta.uid + '" style="' + styleContainer + '"></div>');
+
+	// form
 	var elements = {};
 
 	elements.form = document.createElement('form');
-	elements.form.action = "";
 	elements.form.onsubmit = formSubmit;
 
 	elements.input = document.createElement('input');
+	elements.input.value = "";
+	elements.input.name = "identifier";
+	elements.input.setAttribute("validate", "^.{2,32}$");
 	elements.input.placeholder = "Your name";
+	elements.input.onblur = formValidate;
+	elements.input.onkeyup = formValidate;
 
 	elements.button = document.createElement('button');
-	elements.button.innerHTML = "Start";
+	elements.button.type = "submit";
+	elements.button.name = "submit";
+	elements.button.innerHTML = "Take Survey";
 
 	elements.form.appendChild(elements.input);
 	elements.form.appendChild(elements.button);
-	
-	// WRITE OUTPUT - finished form
-	document.getElementById(meta.id).appendChild(elements.form);
 
+	// WRITE OUTPUT - form
+	document.getElementById(meta.uid).appendChild(elements.form);
 })();
