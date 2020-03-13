2015f90a9baa5ffb7da0453d81bf064aadebd9d1
Paul Shorey <ps@artspaces.net>
2020-03-12 18:09:51 -0700

    fix bug SelectTld focus

--- www/new/build/service-worker.js
+++ www/new/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.909598acfc91741fef388da791711e29.js"
+  "/precache-manifest.b9fc3957291f8c067718832992647433.js"
 );
 
 self.addEventListener('message', (event) => {
