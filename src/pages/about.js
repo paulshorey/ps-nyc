1e174ae8cffbc1d0872a6d33468a6b14bfd3b4f8
Paul Shorey <ps@artspaces.net>
2021-01-02 19:27:15 -0500

    about header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,12 @@
 import React from "react"
 import about_media from "src/html/about/about.html"
+import Header from "../components/Header"
 
 export default function () {
-  return <div dangerouslySetInnerHTML={{ __html: about_media }} />
+  return (
+    <>
+      <Header />
+      <div dangerouslySetInnerHTML={{ __html: about_media }} />
+    </>
+  )
 }
