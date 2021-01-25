5c08333a46a9fad5f8e3083332a8df1dec0c5ad8
Paul Shorey <ps@artspaces.net>
2021-01-24 23:15:43 -0500

    precommit hook

---
+++ bin/test_and_publish.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# OK, there are no tests here. But they're coming. ;)
+
+#
+# GIT and filesystem variables
+#
+branch=$(git symbolic-ref --short HEAD)
+cwd=$(dirname $(realpath $0))
+rootdir=$cwd/..
+#
+# Only publish if on master/main branch!
+#
+if [ "$branch" = "main" ] || [ "$branch" = "master" ]
+then
+  printf "\n\npublishing %s branch\n\n" "$branch"
+  npm version patch --no-git-tag-version &&
+  npm run build &&
+  git add .
+  #
+  # Publish
+  #
+#  rm -rf "$rootdir/../public/ps" &&
+#  mv "$rootdir/public" "$rootdir/../public/ps" &&
+#  cd "$rootdir/../public" &&
+#  git add . &&
+#  git commit -m 'ps' &&
+#  git push
+else
+  #
+  # Do not publish feature branches
+  #
+  printf "\n\nnot on main or master branch\n\n"
+fi
+#
+# Publishing should not block next execution. Only testing/linting can exit with 1+ code
+#
+exit 0
