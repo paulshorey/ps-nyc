91a51e04a49392c49cb30e8db9bbf4fe4276e448
Paul Shorey <ps@artspaces.net>
2021-01-24 22:42:19 -0500

    precommit script

--- bin/precommit.sh
+++ bin/precommit.sh
@@ -1,10 +1,11 @@
 #
-# Custom
+# Custom "publish" script. Only publish if on master/main branch!
 #
 branch=$(git symbolic-ref --short HEAD)
-if [ $branch = main ] || [ $branch = master ]
+if [ "$branch" = "main" ] || [ "$branch" = "master" ]
 then
   npm version patch --no-git-tag-version && npm run build && npm run pub && git add .
 else
-  echo "not on main or master branch"
+  printf "\nnot on main or master branch\n"
 fi
+
